c41ec1da24bc81397278bfd1ce9b1b7f7eb7f0ed
Felix Krause <felix_krause1999@yahoo.de>
2023-11-07 12:00:03 +0100

    update configs and [WIP] MNIST configs

--- configs/poisson/cifar10_ddpmpp.py
+++ configs/poisson/cifar10_ddpmpp.py
@@ -19,62 +19,62 @@ from configs.default_cifar10_configs import get_default_configs
 
 
 def get_config():
-  config = get_default_configs()
-  # training
-  training = config.training
-  training.sde = 'poisson'
-  training.continuous = True
-  training.batch_size = 4096
-  training.small_batch_size = 128
-  training.gamma = 5
-  training.restrict_M = True
-  training.tau = 0.03
-  training.snapshot_freq = 50000
-  training.model = 'ddpmpp'
+    config = get_default_configs()
+    # training
+    training = config.training
+    training.sde = 'poisson'
+    training.continuous = True
+    training.batch_size = 512  # was initially 4096, increase if GPU allows it; used to compute Poisson field
+    training.small_batch_size = 128  # actually used for SGD
+    training.gamma = 5
+    training.restrict_M = True
+    training.tau = 0.03
+    training.snapshot_freq = 500
+    training.model = 'ddpmpp'
 
-  # data
-  data = config.data
-  data.channels = 3
-  data.centered = True
+    # data
+    data = config.data
+    data.channels = 3
+    data.centered = True
 
-  # sampling
-  sampling = config.sampling
-  sampling.method = 'ode'
-  sampling.ode_solver = 'rk45'
-  #sampling.ode_solver = 'forward_euler'
-  #sampling.ode_solver = 'improved_euler'
-  sampling.N = 100
-  sampling.z_max = 40
-  sampling.z_min = 1e-3
-  sampling.upper_norm = 3000
-  # verbose
-  sampling.vs = False
+    # sampling
+    sampling = config.sampling
+    sampling.method = 'ode'
+    sampling.ode_solver = 'rk45'
+    # sampling.ode_solver = 'forward_euler'
+    # sampling.ode_solver = 'improved_euler'
+    sampling.N = 100
+    sampling.z_max = 40
+    sampling.z_min = 1e-3
+    sampling.upper_norm = 3000
+    # verbose
+    sampling.vs = False
 
-  # model
-  model = config.model
-  model.name = 'ncsnpp'
-  model.scale_by_sigma = False
-  model.ema_rate = 0.9999
-  model.normalization = 'GroupNorm'
-  model.nonlinearity = 'swish'
-  model.nf = 128
-  model.ch_mult = (1, 2, 2, 2)
-  model.num_res_blocks = 4
-  model.attn_resolutions = (16,)
-  model.resamp_with_conv = True
-  model.conditional = True
-  model.fir = False
-  model.fir_kernel = [1, 3, 3, 1]
-  model.skip_rescale = True
-  model.resblock_type = 'biggan'
-  model.progressive = 'none'
-  model.progressive_input = 'none'
-  model.progressive_combine = 'sum'
-  model.attention_type = 'ddpm'
-  model.init_scale = 0.
-  model.fourier_scale = 16
-  model.embedding_type = 'positional'
-  model.conv_size = 3
-  model.sigma_end = 0.01
+    # model
+    model = config.model
+    model.name = 'ncsnpp'
+    model.scale_by_sigma = False
+    model.ema_rate = 0.9999
+    model.normalization = 'GroupNorm'
+    model.nonlinearity = 'swish'
+    model.nf = 128
+    model.ch_mult = (1, 2, 2, 2)
+    model.num_res_blocks = 4
+    model.attn_resolutions = (16,)
+    model.resamp_with_conv = True
+    model.conditional = True
+    model.fir = False
+    model.fir_kernel = [1, 3, 3, 1]
+    model.skip_rescale = True
+    model.resblock_type = 'biggan'
+    model.progressive = 'none'
+    model.progressive_input = 'none'
+    model.progressive_combine = 'sum'
+    model.attention_type = 'ddpm'
+    model.init_scale = 0.
+    model.fourier_scale = 16
+    model.embedding_type = 'positional'
+    model.conv_size = 3
+    model.sigma_end = 0.01
 
-  return config
+    return config
